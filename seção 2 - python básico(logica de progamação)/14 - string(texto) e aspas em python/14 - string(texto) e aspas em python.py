d4b63ce68776692baaf6ea3d376437c9561db040
Luiz Gustavo Zanerato Evangelista <gustavozanerato@hotmail.com>
2022-10-08 19:49:13 -0300

    14 - string(texto) e aspas em python

---
+++ seção 2 - python básico(logica de progamação)/14 - string(texto) e aspas em python/14 - string(texto) e aspas em python.py
@@ -0,0 +1,13 @@
+#  str(string): tipo de dado primitivo em python(textos)
+print('isso é uma string')
+
+# Tem que começar e terminar como mesmo tipo de aspas
+print('esse é o meu "texto"')
+
+# caractere de escape
+print('esse é o meu \'texto\'')
+print("esse é o meu \"texto\"")
+
+# quebra de linha
+print('primeira linha.\nsegunda linha.')
+
