1f5e72d38d2d2daaa5f2c0410d8701487209cc25
Luiz Gustavo Zanerato Evangelista <gustavozanerato@hotmail.com>
2022-10-08 18:56:41 -0300

    13 - o comando print

---
+++ seção 2 - python básico(logica de progamação)/13 - O comando print/13 - O comando print.py
@@ -0,0 +1,10 @@
+# qualquer palavra com parenteses no final é referencia a uma função
+print(123)
+
+# sep: separador de argumentos
+# end: juntar códigos em linhas diferentes na mesma linha
+print("luiz", 'gustavo', sep='--', end=" + ")
+print('luiz', 'gustavo')
+
+
+
