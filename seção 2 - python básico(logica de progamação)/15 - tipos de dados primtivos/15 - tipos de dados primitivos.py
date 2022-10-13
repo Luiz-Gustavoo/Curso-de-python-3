97872f9ce0143910ed0e081d47c3e47352b42fce
Luiz Gustavo Zanerato Evangelista <gustavozanerato@hotmail.com>
2022-10-12 21:20:34 -0300

    15 - tipos de dados primitivos

---
+++ seção 2 - python básico(logica de progamação)/15 - tipos de dados primtivos/15 - tipos de dados primitivos.py
@@ -0,0 +1,18 @@
+# str - string
+print('isso é uma string')
+
+# int - inteiro
+print(1, 10, -45, 10000)
+
+# float - ponto flutuante
+print(10.2, 78.56)
+
+# booleano - true ou false
+print(10 == 10)
+
+# type
+print(type('luiz'))
+print(type(123))
+print(type(2.0))
+print(type(True))
+
