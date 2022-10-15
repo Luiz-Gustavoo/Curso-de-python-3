caa24f481c5a69b08894d20360f5647a1835521b
Luiz Gustavo Zanerato Evangelista <gustavozanerato@hotmail.com>
2022-10-15 13:45:49 -0300

    18 - variáveis e IMC

---
+++ seção 2 - python básico(logica de progamação)/18 - variáveis/18 - variaveis.py
@@ -0,0 +1,18 @@
+# variáveis são usadas para armazenar dados
+
+nome = 'luiz'
+altura = 1.68
+peso = 105
+idade = 20
+e_maior = idade > 18
+if e_maior is True:
+    e_maior = 'sim'
+print(e_maior)
+
+
+print(
+    '\n nome:', nome,
+    '\n altura:', altura,
+    '\n peso:', peso,
+    '\n idade:', idade,
+    '\n é maior de idade?:', e_maior)
---
+++ seção 2 - python básico(logica de progamação)/18 - variáveis/IMC.py
@@ -0,0 +1,9 @@
+nome = 'luiz'
+peso = 105
+altura = 1.68
+idade = 20
+imc = peso / altura ** 2
+
+print(nome, 'tem', idade, 'anos e seu imc é:', imc)
+
+
