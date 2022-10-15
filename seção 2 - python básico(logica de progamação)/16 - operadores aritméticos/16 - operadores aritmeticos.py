5d441324df925fbd54ca02bdfeb518cefae0fa8d
Luiz Gustavo Zanerato Evangelista <gustavozanerato@hotmail.com>
2022-10-15 12:44:49 -0300

    16 - operadores aritméticos

---
+++ seção 2 - python básico(logica de progamação)/16 - operadores aritméticos/16 - operadores aritmeticos.py
@@ -0,0 +1,29 @@
+# +, -. /, *, %, **, //
+
+print(10 * 10)
+print(10 + 10)
+
+# divisão arredondada
+print(10 // 2)
+
+# divisão comum
+print(10 / 2)
+
+# multiplacação como multiplicador de string
+print(10 * 'banana ')
+print(10 * '<> ')
+
+# concatenação de strings
+print('200' + '200')
+
+# convertendo tipo de dados
+print(str(20))
+print(type(20))
+
+print(10 % 2)
+
+print(10 ** 2)
+
+# precedência dos operadores aritméticos
+print((10 + 10) * 2)
+
