6740a855eac834658dc6400b5ead468a7e18f08e
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2022-12-06 22:13:51 -0300

    f-strings

---
+++ seção 2 - python básico(logica de progamação)/30 - formatação de strings/f-strings.py
@@ -0,0 +1,12 @@
+nome = 'luiz gustavo'
+idade = 20
+altura = 1.68
+
+f_strings = f'meu nome é {nome}, tenho {idade} anos e tenho {altura} de altura'
+print(f_strings)
+
+# formatar casas decimais
+conta = 250 / 3
+print(conta)
+casas_deci = f'{conta:.2f}' # duas casas depois do ponto
+print(casas_deci)
