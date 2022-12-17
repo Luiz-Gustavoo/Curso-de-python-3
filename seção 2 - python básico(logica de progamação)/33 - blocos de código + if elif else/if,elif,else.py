b5fbe10b53cb9a97fb7e975a1ab042f5bf8912fe
Luiz-Gustavoo <gustavo_luiz45@hotmail..com>
2022-12-17 20:36:03 -0300

    condicionais(if, elif e else)

---
+++ seção 2 - python básico(logica de progamação)/33 - blocos de código + if elif else/if,elif,else.py
@@ -0,0 +1,11 @@
+# if/elif/else
+# se/ se não se/ se não
+
+sistema = input('você deseja entrar ou sair do sistema? ')
+
+if sistema == 'entrar':
+    print('você entrou no sistema')
+elif sistema == 'sair':
+    print('você saiu do sistema')
+else:
+    print('você não digitou nenhuma das duas opções')
