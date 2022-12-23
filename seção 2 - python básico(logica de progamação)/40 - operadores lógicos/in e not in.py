6ebe96f1802bc42e9936a0f4f24c8fd428f734e0
Luiz-Gustavoo <gustavo_luiz45@hotmail..com>
2022-12-23 17:19:46 -0300

    in e not in

---
+++ seção 2 - python básico(logica de progamação)/40 - operadores lógicos/in e not in.py
@@ -0,0 +1,11 @@
+# strings são iteráveis
+# iteraveis: você pode navegar item por item
+#       0123      
+nome = 'luiz'
+print(nome[3]) # indices
+
+print('l' in nome)
+print('b' in nome)
+print('l' not in nome)
+
+
