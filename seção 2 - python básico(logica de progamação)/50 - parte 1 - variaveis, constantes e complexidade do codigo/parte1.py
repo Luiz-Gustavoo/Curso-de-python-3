e4e43a8404bbdb9aa0d1b3829cabb37b7f115dc7
Luiz-Gustavoo <gustavo_luiz45@hotmail..com>
2023-01-01 16:50:57 -0300

    constantes e complexidade do código

---
+++ seção 2 - python básico(logica de progamação)/50 - parte 1 - variaveis, constantes e complexidade do codigo/parte1.py
@@ -0,0 +1,5 @@
+"""
+constante: variáveis que não vão mudar -> são representadas por letra maiúscula
+    <- muitos blocos de código dentro de outros blocos também é ruim(muito complexo)
+"""
+RADAR = 120 # variável constante(não muda)
