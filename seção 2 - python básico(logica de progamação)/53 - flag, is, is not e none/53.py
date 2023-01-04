d29d3f48f8361bc8d3f2a73c6a6ee57123462264
Luiz-Gustavoo <gustavo_luiz45@hotmail..com>
2023-01-03 23:48:47 -0300

    flag, is, is not e none

---
+++ seção 2 - python básico(logica de progamação)/53 - flag, is, is not e none/53.py
@@ -0,0 +1,15 @@
+pedir_senha = input('digite sua senha: ')
+
+
+if pedir_senha is not None:
+    digitou_senha = True
+    print('usuário digitou no campo senha')
+
+if pedir_senha is None:
+    digitou_senha: None
+    print('usuário não digitou no campo senha')
+
+
+
+
+
