c44334fc241e70a48d2fdbce6ba08fd422805270
Luiz-Gustavoo <gustavo_luiz45@hotmail..com>
2023-01-07 20:31:12 -0300

    sabendo se o interpretador passou no if

--- seção 2 - python básico(logica de progamação)/53 - flag, is, is not e none/53.py
+++ seção 2 - python básico(logica de progamação)/53 - flag, is, is not e none/53.py
@@ -1,15 +1,26 @@
+'''
+ saber se o interpretador passou por um local
+
+passou_no_if = None
+input = input('digite sua senha: ')
+
+if input:
+    print(input)
+else:
+    print('input vazio')
+'''
 pedir_senha = input('digite sua senha: ')
+digitou_senha = None
 
-
-if pedir_senha is not None:
-    digitou_senha = True
+if pedir_senha:
     print('usuário digitou no campo senha')
+    digitou_senha = True    
+else:
+    print('usuário não digitou no campo senha') 
 
-if pedir_senha is None:
-    digitou_senha: None
-    print('usuário não digitou no campo senha')
+if digitou_senha is not None:
+    print('passou no if')
 
-
-
-
+if digitou_senha is None:
+    print('não passou no if')  
 
