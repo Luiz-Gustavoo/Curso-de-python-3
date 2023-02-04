be0b3e28ab6b059e0099f2435943b85863755f6b
Luiz-Gustavoo <gustavo_luiz45@hotmail..com>
2023-02-04 20:46:52 -0300

    while: continue + brake

---
+++ seção 2 - python básico(logica de progamação)/62 - while e continue/62.py
@@ -0,0 +1,24 @@
+contador_idades = 0
+contador_idades_int = int(contador_idades)
+
+while contador_idades_int <=30:
+    idade = input('qual a sua idade?: ')
+    idade_int = int(idade)
+
+    contador_idades_int =+1
+    print(f'contador: {contador_idades_int}')
+
+    if idade_int == 20:
+        print('20 é inválido')
+        continue
+    
+    print(f'idade: {idade_int}')
+
+
+    if idade_int == 25:
+        break
+
+
+print('acabou') 
+
+
