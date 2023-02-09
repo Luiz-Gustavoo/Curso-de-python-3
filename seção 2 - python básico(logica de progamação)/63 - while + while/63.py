d5b4178c14e47cf816c7dac92471abb4f690c7ac
Luiz Gustavo Zanerato Evangelista <gustavozanerato@hotmail.com>
2023-02-08 22:07:44 -0300

    while + while

---
+++ seção 2 - python básico(logica de progamação)/63 - while + while/63.py
@@ -0,0 +1,12 @@
+qtd_numero1 = 5
+qtd_numero2 = 5
+
+numero1 = 1
+while numero1 <= qtd_numero1:
+    numero2 = 1
+    while numero2 <= qtd_numero2:
+        print(numero1, numero2)
+        numero2 += 1
+    numero1 += 1    
+
+print('acabou')
