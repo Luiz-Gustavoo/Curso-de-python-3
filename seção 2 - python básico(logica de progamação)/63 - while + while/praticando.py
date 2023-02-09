ec0f7033a7fdf4df646639af1ddd15f9e2c17253
Luiz Gustavo Zanerato Evangelista <gustavozanerato@hotmail.com>
2023-02-08 22:08:07 -0300

    pergunta e resposta básico usando while

---
+++ seção 2 - python básico(logica de progamação)/63 - while + while/praticando.py
@@ -0,0 +1,17 @@
+numero_tentativas = 3
+
+while numero_tentativas > 0:
+    print('vamos começar nosso teste')
+    print(f'você tem mais {numero_tentativas} tentativas')
+
+    pergunta1 = int(input('quanto é 2 + 2?: '))
+    resposta_pergunta1 = 4
+
+    if pergunta1 == resposta_pergunta1:
+        print('você acertou')
+    else:
+        print('você errou')
+        numero_tentativas -=1
+    if numero_tentativas <= 0:
+        print('suas tentativas acabaram')
+        
