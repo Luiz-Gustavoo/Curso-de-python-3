4387bd44c6ecbdb5718b0c4e0008ea0087aa382c
Luiz Gustavo Zanerato Evangelista <gustavozanerato@hotmail.com>
2023-02-14 21:25:14 -0300

    iterando uma string com while

---
+++ seção 2 - python básico(logica de progamação)/64 - exercicio com while/64.py
@@ -0,0 +1,22 @@
+"""
+iterando uma string com while
+"""
+#       01234567891011
+nome = 'luiz gustavo'
+len_nome = len(nome)
+contador_indice = 0
+
+
+nome_formatado = ''
+len_nome_formatado = len(nome_formatado)
+
+while len_nome_formatado < len_nome:
+    if len(nome) == len(nome_formatado):
+        break
+    indice_nome = nome[contador_indice]
+    nome_formatado += indice_nome
+    contador_indice +=1
+    
+    print(nome_formatado)    
+    
+
