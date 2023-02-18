cbe2fc1a15347947da9ac2bd2f40caafef7c2896
Luiz-Gustavoo <gustavo_luiz45@hotmail..com>
2023-02-18 16:42:39 -0300

    iterando uma string com while

--- seção 2 - python básico(logica de progamação)/64 - exercicio com while/64.py
+++ seção 2 - python básico(logica de progamação)/64 - exercicio com while/64.py
@@ -3,23 +3,17 @@ iterando uma string com while
 """
 #       01234567891011
 nome = 'luiz gustavo'
-len_nome = len(nome)
 contador_indice = 0
-
-
 nome_formatado = ''
-len_nome_formatado = len(nome_formatado)
 
-while len_nome_formatado < len_nome:
-    if len(nome_formatado) == len(nome):
-        break
+
+while contador_indice < len(nome):
+    letra = nome[contador_indice]
+    nome_formatado += f'-{letra} '
     
-    indice_nome = nome[contador_indice]
-    nome_formatado += indice_nome
     contador_indice +=1
-
-    if len(nome_formatado) == len(nome):
-        print(nome_formatado)
+    
+print(nome_formatado)    
     
     
 
