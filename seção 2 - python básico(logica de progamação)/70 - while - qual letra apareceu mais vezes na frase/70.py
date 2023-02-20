a9f8dd1107847f863a4eaa66e76d4e6e0f434243
Luiz-Gustavoo <gustavo_luiz45@hotmail..com>
2023-02-20 20:29:13 -0300

    parte 1

---
+++ seção 2 - python básico(logica de progamação)/70 - while - qual letra apareceu mais vezes na frase/70.py
@@ -0,0 +1,11 @@
+frase = 'o rato roeu a roupa do rei de roma'
+
+contador_indice = 0
+
+while contador_indice < len(frase):
+    letra_atual = frase[contador_indice]
+
+    print('{letra_atual}')
+    
+    contador_indice += 1
+
