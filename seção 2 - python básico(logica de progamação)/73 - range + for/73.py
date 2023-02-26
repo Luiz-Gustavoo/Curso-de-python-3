7bbdb48a866a3f30fc86e21544d5f06d2dfb89c1
Luiz-Gustavoo <gustavo_luiz45@hotmail..com>
2023-02-26 20:47:40 -0300

    range + for

---
+++ seção 2 - python básico(logica de progamação)/73 - range + for/73.py
@@ -0,0 +1,6 @@
+# range: cria uma variavel com números até um determinado valor definido
+
+numeros = range(10)
+
+for numero in numeros: # pra cada iteração, vai colocar um numero em "numero"
+    print(numero)
