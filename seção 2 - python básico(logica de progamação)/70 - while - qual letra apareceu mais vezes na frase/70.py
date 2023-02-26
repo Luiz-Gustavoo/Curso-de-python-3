41f670ba6a1e8cfec7414fde2384771fdb15b0a3
Luiz-Gustavoo <gustavo_luiz45@hotmail..com>
2023-02-26 19:58:16 -0300

    qual letra apareceu mais na frase. obs: não consegui fazer sozinho

--- seção 2 - python básico(logica de progamação)/70 - while - qual letra apareceu mais vezes na frase/70.py
+++ seção 2 - python básico(logica de progamação)/70 - while - qual letra apareceu mais vezes na frase/70.py
@@ -1,11 +1,25 @@
-frase = 'o rato roeu a roupa do rei de roma'
+frase = 'laaa'
 
 contador_indice = 0
+qtd_apareceu_mais = 0
+letra_apareceu_mais = ''
 
 while contador_indice < len(frase):
     letra_atual = frase[contador_indice]
 
-    print('{letra_atual}')
-    
-    contador_indice += 1
+    if letra_atual == ' ':
+        contador_indice+=1
+        continue
 
+    quantas_vezez_apareceu = frase.count(letra_atual)
+
+    if qtd_apareceu_mais < quantas_vezez_apareceu:
+        letra_apareceu_mais = letra_atual
+        qtd_apareceu_mais = quantas_vezez_apareceu
+        
+    
+    contador_indice+=1
+        
+    
+
+print(letra_apareceu_mais)
