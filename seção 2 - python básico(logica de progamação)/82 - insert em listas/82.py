b19fe00c2d3c0cc727e9c8f85b87e0bf469b7656
Luiz-Gustavoo <gustavo_luiz45@hotmail..com>
2023-03-17 23:18:01 -0300

    insert em listas

---
+++ seção 2 - python básico(logica de progamação)/82 - insert em listas/82.py
@@ -0,0 +1,10 @@
+# insert: adiciona um valor em um determinado indice
+# append: cria um indice no final da lista e adiciona um valor nele
+
+lista = [1, 2, 3, 4]
+#           indice  valor
+lista.insert(0, 9)
+print(lista)
+
+lista.clear()
+print(lista)
