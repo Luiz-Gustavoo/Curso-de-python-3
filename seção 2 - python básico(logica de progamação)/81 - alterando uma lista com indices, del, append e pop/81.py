9832490f56aec4285bf4170e8b04de384e6b229a
Luiz-Gustavoo <gustavo_luiz45@hotmail..com>
2023-03-17 23:06:26 -0300

    métodos com listas

---
+++ seção 2 - python básico(logica de progamação)/81 - alterando uma lista com indices, del, append e pop/81.py
@@ -0,0 +1,13 @@
+# CRUD: create, read, update e delete
+
+lista = [1, 2, 3, 4]
+
+del lista [0] # deletando indice e valor 
+print(lista)
+
+lista.append(5)
+print(lista) # adicionando valores no final da lista
+
+
+lista.pop() # remove o ultimo item da lista
+print(lista)
