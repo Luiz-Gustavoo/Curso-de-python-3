01f2efc37dbe257d7a1cbee4713767d6cd34a69b
Luiz-Gustavoo <gustavo_luiz45@hotmail..com>
2023-07-22 21:17:40 -0300

    tuplas

---
+++ seção 2 - python básico(logica de progamação)/88 - tipo tuple(tuplas)/88.py
@@ -0,0 +1,11 @@
+"""
+tupla é uma lista imutável
+"""
+"""
+TypeError: 'tuple' object does not support item assignment
+"""
+tupla_nomes = 'maria', 'luiz', 'helena'
+print(tupla_nomes)
+
+tupla_frutas = ('banana', 'maçã', 'laranja')
+print(tupla_frutas)
