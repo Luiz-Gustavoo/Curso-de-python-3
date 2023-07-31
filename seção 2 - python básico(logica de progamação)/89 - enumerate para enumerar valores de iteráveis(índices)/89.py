7ba9413536f436e8dcea2752bb0d020b397d56b1
Luiz-Gustavoo <gustavo_luiz45@hotmail..com>
2023-07-31 20:41:37 -0300

    enumerate

---
+++ seção 2 - python básico(logica de progamação)/89 - enumerate para enumerar valores de iteráveis(índices)/89.py
@@ -0,0 +1,42 @@
+"""
+o enumerate irá colocar os respectivos índices de valores ao lado deles
+só pode ser usado uma vez
+"""
+#           0        1        2
+lista = ['maria', 'helena', 'luiz']
+lista_enumerada = tuple(enumerate(lista))
+print(lista_enumerada)
+#        0              1           2
+#   0       1     0     1        0     1
+# ((0, 'maria'), (1, 'helena'), (2, 'luiz'))
+
+
+print('ENUMERATE PADRÃO:')
+for valores_enumerados in lista_enumerada:
+   print(valores_enumerados)
+
+for valores_enumerados in lista_enumerada:
+   print(valores_enumerados)
+
+
+ #usar o enumerate mais de uma vez: enumerate direto no for
+
+print('ENUMERATE DIRETO NO FOR:')
+for valores_enumerados in enumerate(lista):
+   print(valores_enumerados)
+
+for valores_enumerados in enumerate(lista):
+   print(valores_enumerados)
+
+
+ #usar o enumerate mais de uma vez: type conversion
+
+print('TYPE CONVERSION:')
+lista_enumerada = list(enumerate(lista))
+print(lista_enumerada)
+
+
+for indice, nome in enumerate(lista):
+   print(indice, nome)
+
+
