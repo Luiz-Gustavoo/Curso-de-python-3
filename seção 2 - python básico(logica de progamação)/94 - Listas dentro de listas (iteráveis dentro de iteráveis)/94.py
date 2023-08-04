8d6c15ab616efdaed5271b3394a1dca049b72dc9
Luiz-Gustavoo <gustavo_luiz45@hotmail..com>
2023-08-04 17:33:30 -0300

    iteráveis dentro de iteráveis

---
+++ seção 2 - python básico(logica de progamação)/94 - Listas dentro de listas (iteráveis dentro de iteráveis)/94.py
@@ -0,0 +1,17 @@
+salas_primeiro_ano = [
+    #   0         1
+    ['Maria', 'Helena'], # 0
+    #   0
+    ['Elaine'], # 1
+    #   0       1       2 
+    ['Luiz', 'João', 'Eduarda'] # 2
+]
+for sala in salas_primeiro_ano:
+    print('-' * 10)
+    print(f'Alunos da sala atual:')
+    for alunos in sala:
+        print(alunos)
+        
+    
+
+    
