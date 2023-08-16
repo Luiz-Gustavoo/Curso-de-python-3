db23b264998afee735a2ae49b81c8a670dabe877
Luiz-Gustavoo <gustavo_luiz45@hotmail..com>
2023-08-16 20:05:09 -0300

    introdução a funções

---
+++ seção 3 - python intermediário(programação procedural)/105.py
@@ -0,0 +1,14 @@
+def Print():
+    print('função')
+
+Print()
+
+def soma(a, b, c):
+    soma_valores = a + b + c
+    print(soma_valores)
+soma(2, 4, 6)
+soma(1, 3, 5)
+
+def saudacao(nome='Sem nome'):
+    print(f'Olá "{nome}"')
+saudacao()
